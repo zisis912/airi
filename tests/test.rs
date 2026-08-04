13bca3ecae4ffecea5f23523a1d75a6a71333ecb
zisis912 <gtf2688@gmail.com>
2026-08-04 09:26:05 +0300

    moved rsa key to its own file

--- tests/test.rs
+++ tests/test.rs
@@ -1,43 +1,63 @@
-use std::error::Error;
+use std::{error::Error, fs::File};
 
 use mc_rust_protocol::{
     RawPacket,
     packet::{
         self, Direction, Packet, State,
     },
-    packet_decoder::NetworkDecoder,
+    packet_decoder::{NetworkDecoder, PacketDecodeError},
 };
 use rsa::{
     Pkcs1v15Encrypt, RsaPrivateKey,
     pkcs8::DecodePrivateKey,
 };
+use thiserror::Error;
 
+#[derive(Error, Debug)]
+enum TestError {
+    #[error("couldnt obtain aes key from c2s EncryptionResponse packet")]
+    AesKeyMissing
+}
 
 #[test]
 fn testing() {
-    let _ = sample_data(Direction::Clientbound);
-    let _ = sample_data(Direction::Serverbound);
+    // first decrypt aes key as the server, then use it in the client
+    let mut aes_key = None;
+
+    let _= sample_data(Direction::Serverbound, &mut aes_key);
+    let _= sample_data(Direction::Clientbound, &mut aes_key);
 }
 
-fn sample_data(decrypt_dir: Direction) -> Result<(), Box<dyn Error>> {
-    // let c2s = File::open("tests/sample_data/C2S.bin")?;
-    let c2s = include_bytes!("sample_data/C2S.bin");
-    // let s2c = File::open("tests/sample_data/S2C.bin")?;
-    let s2c = include_bytes!("sample_data/S2C.bin");
+fn sample_data(decrypt_dir: Direction, aes_key_g: &mut Option<[u8;16]>) -> Result<(), Box<dyn Error>> {
+    let c2s = File::open("tests/sample_data/C2S.bin").unwrap();
+    let s2c = File::open("tests/sample_data/S2C.bin").unwrap();
 
-    let server_private_key = RsaPrivateKey::from_pkcs8_der(&hex::decode("30820276020100300d06092a864886f70d0101010500048202603082025c02010002818100b8e9bff8624c3ae888ab0cfceeebdc509d452f1a15a140614a5cc3b6387120649da5d53b6b3fe250d07d18ccca0bf14467cd8217346bdbfe7a6ff1736ecfac80d8bcb80940be1cb859e0a33ff1814664dd46defddda6fa3abdd063ca6e933da9cb2710e4b17b5b4cf96ac0fa9b8d1d780105db1b471e77ea3de3a87d373772e10203010001028180549547e4bc4216682babe2a3083f076630aa66e34da5972769b689279f25d025761f572c78e09e0b4d730b97118ce8eddb759bde3572690d3cc05bf7eb663f875f8343a634f33bb87f99f73f6ab95e042e2543d0d4b777e090de457bf8d409e1b65469f9c952a98f3cb0217557a06f1d2729469c57562935fc01152dfc38b509024100fb24a657710b710072a0da2a9637a08ea559c1c85a89c46c526520540ae0a8302b0f0c47c5857d94d2301d2d6a6df58631d28193ca039440b5b1e6e910100a6b024100bc7d35f0ee4f0209f5216db2ac5b1d0dac57bd64d9e413413c94a914e1b530e17b6c78d5cd29ad443c1231f0666064688f03690eb8e0bf3f8736342765d422e30240290e92cb14c6041148ac173e8314510140f2ed852d97fc2ea141bb094245fbf8f3f11fd6d3e9c0e00584ac207297cb5dc6e35d1fa614f3b5a87e8efb670ed845024027394f2e520932fd6b7b875e752b88c23da90c8a9e252e34972cc07acdf56cb49f80952cb8c301817f96b1b9bb3437f0e241ed6cd8e03c2c3630fb6d6f6d53cd024100a80c554e9f3c4e6e3854c01894f6993a336fd6675912089db1a7c7a98a161f1d4009b526d4a7b0caa1f607af5587778f4de0eee9ac887f4ab4317d22dbaf1cca").unwrap()).unwrap();
-    // let mut server_public_key: Option<RsaPublicKey> = None;
+
+    // the last char of this string must be removed, it took me 1 hour to find this
+    let key =include_str!("sample_data/rsa_key.txt");
+
+    println!("{}{:x}",key.len(),key.chars().nth(0).unwrap()as i32);
+    let server_private_key = RsaPrivateKey::from_pkcs8_der(&hex::decode(&key[0..key.len()-1]).unwrap()).unwrap();
 
     let (decoder, mut state) = match decrypt_dir {
-        Direction::Clientbound => (&mut NetworkDecoder::new(&s2c[..]), State::Login),
-        Direction::Serverbound => (&mut NetworkDecoder::new(&c2s[..]), State::Handshake),
+        Direction::Clientbound => (&mut NetworkDecoder::new(&s2c), State::Login),
+        Direction::Serverbound => (&mut NetworkDecoder::new(&c2s), State::Handshake),
     };
 
     println!("DIRECTION: {:?}", decrypt_dir);
     println!("setting state to login");
 
     loop {
-        let RawPacket { id, payload } = decoder.get_raw_packet()?;
+        let res =decoder.get_raw_packet();
+        match res {
+            Err(PacketDecodeError::FailedDecompression(ref e)) => {
+                if e == "IO error: failed to fill whole buffer" {
+                    return Ok(());
+                }
+            },
+            _=>{}
+        }
+        let RawPacket { id, payload } = res.unwrap();
 
         println!("id: {:#04x}", id);
         println!("length: {}", payload.len());
@@ -50,21 +70,17 @@ fn sample_data(decrypt_dir: Direction) -> Result<(), Box<dyn Error>> {
             Packet::Handshake(p) => state = p.intent.into(),
             Packet::EncryptionRequest(_p) => {
                 // server_public_key =
-                //     Some(RsaPublicKey::from_public_key_der(&p.public_key.data)?);
-                let aes_key = hex::decode("7532710be168544415a69d2a122b4230")
-                    .unwrap()
-                    .try_into()
-                    .unwrap();
+                //     Some(RsaPublicKey::from_public_key_der(&p.public_key.data).unwrap());
+                // let aes_key = hex::decode("7532710be168544415a69d2a122b4230").unwrap().try_into().map_err(|_|TestError::InvalidAesKeyLength)?;
 
-                decoder.set_encryption(&aes_key);
+                // unwrap cannot fail realistically
+                decoder.set_encryption(&aes_key_g.ok_or(TestError::AesKeyMissing).unwrap());
             }
             Packet::EncryptionResponse(p) => {
                 let aes_key: [u8; 16] = server_private_key
-                    .decrypt(Pkcs1v15Encrypt, &p.shared_secret.data)
-                    .unwrap()[0..16]
-                    .try_into()
-                    .unwrap();
+                    .decrypt(Pkcs1v15Encrypt, &p.shared_secret.data).unwrap()[0..16].try_into()?;
                 println!("acquired AES key: {:#?}", hex::encode(aes_key));
+                *aes_key_g = Some(aes_key);
                 decoder.set_encryption(&aes_key);
                 decoder.set_compression(256);
             }
